331efea375b414800f93e651f8e6f8d7edb99802
lihuosheng-top <1061525487@qq.com>
2018-11-26 20:32:07 +0800

    微信

--- application/index/controller/WechatPay.php
+++ application/index/controller/WechatPay.php
@@ -56,8 +56,9 @@ class WechatPay extends Controller
         $data["sign"] = $sign;
 
         $xml = $this->arrayToXml($data);  //数组转化为xml
-        return ajax_success('数据返回',$xml);
+
         $response = $this->postXmlCurl($xml, $url); //以post方式提交xml到对应的接口url
+        return ajax_success('数据返回',$response);
         $response = $this->xmlToArray($response);  //将xml转为array
         $response = $this->two_sign($response, $data["nonce_str"]); //微信支付二次签名
 
