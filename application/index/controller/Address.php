cc9cf0dc1b90b9b623c35ff3d94c345c2d83c0b5
lihuosheng-top <1061525487@qq.com>
2018-12-26 17:14:17 +0800

    地址管理

--- application/index/controller/Address.php
+++ application/index/controller/Address.php
@@ -58,13 +58,14 @@ class Address extends  Controller{
             $harvester = $request->only('harvester')['harvester'];
             $harvester_phone_num = $request->only('harvester_phone_num')['harvester_phone_num'];
             $address_name = $request->only('address_name')['address_name'];
+            $address =explode(",",$address_name);
             $status = $request->only('status')['status'];
             $harvester_real_address =$request->only('harvester_real_address')['harvester_real_address'];
             $data =[
                 "user_id"=> $member_id ,
                 "harvester"=>$harvester,
                 "harvester_phone_num"=>$harvester_phone_num,
-                "address_name"=>$address_name,
+                "address_name"=> $address,
                 "status"=> $status,
                 "harvester_real_address"=>$harvester_real_address
             ];
@@ -131,7 +132,6 @@ class Address extends  Controller{
             $id = $request->only(['id'])['id'];
             $data =Db::name("user_address")->where('id',$id)->find();
             if(!empty($data)){
-                Session::delete("address_id");
                 return ajax_success('地址信息返回成功',$data);
             }else{
                 return ajax_error('地址信息返回失败',['status'=>0]);
