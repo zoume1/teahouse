9dc7e060b99a8dd57a50882ff4ebcf47f3410c79
lihuosheng-top <1061525487@qq.com>
2018-12-27 17:15:47 +0800

    茶仓订单

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -10,18 +10,50 @@ namespace  app\index\controller;
 
 use think\Controller;
 use think\Request;
+use think\Db;
 
-class  Order extends  Controller{
+class  Order extends  Controller
+{
     /**
      **************李火生*******************
      * @param Request $request
      * Notes:立即购买过去购物清单数据返回
      **************************************
      */
-    public function order_return(Request $request){
-        if($request->isPost()){
-            //goods_id
-            $goods_id =$request->only("goods_id")["goods_id"];
+    public function order_return(Request $request)
+    {
+        if ($request->isPost()) {
+            $goods_id = $request->only("goods_id")["goods_id"];
+            $special_id = $request->only("guige")["guige"];
+            $number = $request->only("num")["num"];
+            if (empty($goods_id)) {
+                return ajax_error("商品信息有误，请返回重新提交", ["status" => 0]);
+            }
+            $goods_data = Db::name("goods")->where("id", $goods_id)->find();
+            //判断是为专用还是通用
+            //专用规格
+            if (!empty($special_id)) {
+                if ($goods_data["goods_standard"] == 1) {
+                    $data["goods_info"] = $goods_data;
+                    $data["special_info"] = Db::name("special")
+                        ->where("id", $special_id)
+                        ->find();
+                    $data["number"] =$number;
+                }
+            } else {
+                //通用规格
+                if ($goods_data["goods_standard"] == 0) {
+                    $data["goods_info"] = $goods_data;
+                    $data["special_info"] = null;
+                    $data["number"] =$number;
+                }
+
+            }
+            if(!empty($data)){
+                return ajax_success("数据返回",$data);
+            }else{
+                return ajax_error("没有数据",["status"=>0]);
+            }
 
         }
     }
