57c2d95624fd3f12826e92f4cbd394c6b927cbbc
lihuosheng-top <1061525487@qq.com>
2018-12-27 11:44:56 +0800

    laqu

--- application/index/controller/Address.php
+++ application/index/controller/Address.php
@@ -131,7 +131,9 @@ class Address extends  Controller{
         if($request->isPost()){
             $id = $request->only(['id'])['id'];
             $data =Db::name("user_address")->where('id',$id)->find();
-            $data["address"] =explode(",",$data['address_name']);
+            foreach ($data["address_name"] as $k=>$v){
+                $data["address"][] =explode(",",$v);
+            }
             if(!empty($data)){
                 return ajax_success('地址信息返回成功',$data);
             }else{
