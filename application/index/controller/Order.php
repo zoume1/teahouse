3d26ce0015ed5eabf6d3f433ecc9696ff7559997
lihuosheng-top <1061525487@qq.com>
2018-12-28 18:06:49 +0800

    茶仓订单

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -42,21 +42,21 @@ class  Order extends  Controller
                 //判断是为专用还是通用
                 //专用规格
                 if ($goods_data["goods_standard"] == 0) {
-                    $data[$key]["goods_info"] = $goods_data[$key];
+                    $data[$key]["goods_info"] = $goods_data;
                     $data[$key]["grade_price"] =$member_consumption_discount["member_consumption_discount"] * $goods_data[$key]["goods_new_money"];
                     $data[$key]["special_info"] = null;
                     $data[$key]["number"] =$number[$key];
                     $data[$key]["user_grade_image"] =$member_consumption_discount["member_grade_img"];
                     } else{
                     $data[$key]["goods_info"] = $goods_data;
-                    if(!empty($special_id[$key])){
+                    if($special_id[$key] != 0){
                         $info = Db::name("special")
                             ->where("id", $special_id[$key])
                             ->find();
                         $data[$key]["special_info"] =$info;
                         $data[$key]["grade_price"] =$member_consumption_discount["member_consumption_discount"]* $info["price"];
                     }else{
-                        $data[$key]["goods_info"] = $goods_data[$key];
+                        $data[$key]["goods_info"] = $goods_data;
                         $data[$key]["grade_price"] =$member_consumption_discount["member_consumption_discount"] * $goods_data[$key]["goods_new_money"];
                     }
                     $data[$key]["number"] =$number[$key];
