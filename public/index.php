7dea17f93ff56e4f7610cb73b4810c25dc1350fd
lihuosheng-top <1061525487@qq.com>
2019-01-07 16:11:25 +0800

    laqu

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('EXTEND_PATH', '../extend/');
+define('EXTEND_PATH',__DIR__ . '/../extend/');
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
