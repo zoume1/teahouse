1652c77d4bff348b5d347cb85c3350d5f4568731
lihuosheng-top <1061525487@qq.com>
2019-01-07 15:48:47 +0800

    laqu

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -1,9 +1,10 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-include('../extend/WxpayAPI/lib/WxPay.Api.php');
+
 class Pay extends Controller {
     function index() {
+        include('../extend/WxpayAPI/lib/WxPay.Api.php');
         //         初始化值对象
         $input = new \WxPayUnifiedOrder();
         //         文档提及的参数规范：商家名称-销售商品类目
@@ -25,6 +26,7 @@ class Pay extends Controller {
     }
     private function getJsApiParameters($UnifiedOrderResult)
     {    //判断是否统一下单返回了prepay_id
+        include('../extend/WxpayAPI/lib/WxPay.Api.php');
         if(!array_key_exists("appid", $UnifiedOrderResult)
             || !array_key_exists("prepay_id", $UnifiedOrderResult)
             || $UnifiedOrderResult['prepay_id'] == "")
