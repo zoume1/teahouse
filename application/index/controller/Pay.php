a45be3171275e73360d656d336c3bb01b61fff62
lihuosheng-top <1061525487@qq.com>
2019-01-07 15:53:12 +0800

    laqu

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -4,7 +4,7 @@ use think\Controller;
 
 class Pay extends Controller {
     function index() {
-        include('../extend/WxpayAPI/lib/WxPay.Api.php');
+//        include('../extend/WxpayAPI/lib/WxPay.Api.php');
         //         初始化值对象
         $input = new \WxPayUnifiedOrder();
         //         文档提及的参数规范：商家名称-销售商品类目
