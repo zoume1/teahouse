41f552b47a05a062731add207f0c93e06b4802c2
guoyangzt <gy156789@163.com>
2019-01-14 18:58:46 +0800

    拉代码

--- application/index/controller/Coupon.php
+++ application/index/controller/Coupon.php
@@ -88,14 +88,17 @@ class Coupon extends Controller
      */
     public function coupon_time(Request $request)
     {
-        if ($request->isPost()) {
-
-            $tea = Db::name("goods_type")->field('name,icon_image,color,id')->where('pid', 0)->where("status", 1)->select();
-            foreach($tea as $key => $value){
-                $res = db("goods_type")->where("pid",$value['id'])->field("name,id")->find();
-                $tea[$key]["tid"] = $res["id"];
-                $tea[$key]["activity_name"] = $res["name"];
-               
+        if ($request->isGet()) {
+            $time = time();//当前时间戳
+            //$member_grade_name = $request->only(['member_grade_name'])['member_grade_name'];
+           // $open_id = $request->only(['open_id'])['open_id'];
+            $coupon = Db::name("coupon")->where('end_time','>=','$time')->select();
+            halt($coupon);
+            foreach($coupon as $key => $value){
+                $coupon[$key]['scope'] = explode(",",$coupon[$key]['scope']);
+                if(in_array($member_grade_name,$coupon[$key]['scope'])){
+                    $data[] = $coupon[$key];
+                }
             }
             if (!empty($tea)) {
                 return ajax_success('传输成功', $tea);
