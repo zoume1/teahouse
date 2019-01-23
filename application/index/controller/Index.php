1ff6238e654abbd8f8652bea46cf9d9cf35d530e
youdianku <571851168@qq.com>
2019-01-23 18:22:19 +0800

    push

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,4 +18,33 @@ class Index extends Controller
         return view("text");
     }
 
+    // 茶厂
+    public function tea_factory(){
+        return view("teafactory");
+    }
+    // 茶商
+    public function tea_merchant(){
+        return view("teamerchant");
+    }
+    // 茶圈
+    public function tea_moment(){
+        return view("teamoment");
+    }
+    // 用户
+    public function consumer(){
+        return view("consumer");
+    }
+    // 智慧茶仓
+    public function wisdom(){
+        return view("wisdom");
+    }
+    // 招募合伙人
+    public function partner(){
+        return view("partner");
+    }
+    // 关于我们
+    public function about(){
+        return view("about");
+    }
+
 }
