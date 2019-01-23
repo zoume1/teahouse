48d93f351c85942ffa715386a7a722c0fe99059b
lihuosheng-top <1061525487@qq.com>
2019-01-23 15:23:14 +0800

    laqu

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -78,22 +78,23 @@ class Pay extends  Controller{
      **************************************
      */
     public function notify(Request $request){
-        $input = new \WxPayOrderQuery();
-        $input->SetTransaction_id();
-        $result = \WxPayApi::orderQuery($input);
-        \Log::DEBUG("query:" . json_encode($result));
-        if(array_key_exists("return_code", $result)
-            && array_key_exists("result_code", $result)
-            && $result["return_code"] == "SUCCESS"
-            && $result["result_code"] == "SUCCESS")
-        {
-            Db::name("activity_order")->where("id",8)->update(["status"=>1]);
-            Db::name("activity_order")->where("parts_order_number",$result["transaction_id"])->update(["status"=>1]);
-        }else{
-            Db::name("activity_order")->where("id",9)->update(["status"=>1]);
-            Db::name("activity_order")->where("parts_order_number",$result["out_trade_no"])->delete();
-            Db::name("activity_order")->where("parts_order_number",$result["transaction_id"])->delete();
-        }
+        Db::name("activity_order")->where("id",8)->update(["status"=>1]);
+//        $input = new \WxPayOrderQuery();
+//        $input->SetTransaction_id();
+//        $result = \WxPayApi::orderQuery($input);
+//        \Log::DEBUG("query:" . json_encode($result));
+//        if(array_key_exists("return_code", $result)
+//            && array_key_exists("result_code", $result)
+//            && $result["return_code"] == "SUCCESS"
+//            && $result["result_code"] == "SUCCESS")
+//        {
+//            Db::name("activity_order")->where("id",8)->update(["status"=>1]);
+//            Db::name("activity_order")->where("parts_order_number",$result["transaction_id"])->update(["status"=>1]);
+//        }else{
+//            Db::name("activity_order")->where("id",9)->update(["status"=>1]);
+//            Db::name("activity_order")->where("parts_order_number",$result["out_trade_no"])->delete();
+//            Db::name("activity_order")->where("parts_order_number",$result["transaction_id"])->delete();
+//        }
     }
 
 //    public function Queryorder($transaction_id)
