e78078792adbfdac24b3ab83736201397230399a
lihuosheng-top <1061525487@qq.com>
2019-01-24 14:05:14 +0800

    laqu

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -78,6 +78,7 @@ class Pay extends  Controller{
      */
 
     public function notify(Request $request){
+        include EXTEND_PATH."WxpayAPI/lib/WxPay.Data.php";
         $input = new \WxPayOrderQuery();
         $input->SetTransaction_id();
         $result = \WxPayApi::orderQuery($input);
@@ -96,40 +97,6 @@ class Pay extends  Controller{
         }
     }
 
-
-//    public function Queryorder($transaction_id)
-//    {
-//        $input = new \WxPayOrderQuery();
-//        $input->SetTransaction_id($transaction_id);
-//        $result = \WxPayApi::orderQuery($input);
-//        if(array_key_exists("return_code", $result)
-//            && array_key_exists("result_code", $result)
-//            && $result["return_code"] == "SUCCESS"
-//            && $result["result_code"] == "SUCCESS")
-//        {
-//            Db::name("activity_order")->where("parts_order_number",$result["out_trade_no"])->update(["status"=>1]);
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//
-//    public function notify($data, &$msg)
-//    {
-//        Db::name("activity_order")->where("parts_order_number","20190123146389")->update(["status"=>1]);
-//        $notfiyOutput = array();
-//        if(!array_key_exists("transaction_id", $data)){
-//            $msg = "输入参数不正确";
-//            return false;
-//        }
-//        //查询订单，判断订单真实性
-//        if(!$this->Queryorder($data["transaction_id"])){
-//            $msg = "订单查询失败";
-//            return false;
-//        }
-//        Db::name("activity_order")->where("parts_order_number","20190123146389")->update(["status"=>1]);
-//        return true;
-//    }
 
 
 
