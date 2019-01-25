9c70edaea90b8b7e60bc2616978175d94f29497f
lihuosheng-top <1061525487@qq.com>
2019-01-25 10:03:41 +0800

    laqu

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -81,8 +81,8 @@ class  Order extends  Controller
      */
     public function order_place(Request $request){
         if ($request->isPost()) {
-            $open_id = $request->param("open_id");//open_id
-            dump($open_id);
+//            $open_id = $request->param("open_id");//open_id
+            $open_id = $request->only(["open_id"])["open_id"];//open_id
             $address_id = $request->param("address_id");//address_id
             $user_id =Db::name("member")
                 ->where("member_openid",$open_id)
