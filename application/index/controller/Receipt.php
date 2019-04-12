2ad0ca500ba8cb82e247945087c25098e30d94be
guoyangzt <gy156789@163.com>
2019-04-12 16:24:24 +0800

    拉代码

--- application/index/controller/Receipt.php
+++ application/index/controller/Receipt.php
@@ -19,7 +19,9 @@ class Receipt extends Controller
      */
     public function bill(Request $request){
         if($request->isPost()){
+            $time = time();
             $data = $request->param();
+            $data["create_time"] = $time;
             if(!empty($data)){
                 $bool = db("member_receipt")->insert($data);
                 return ajax_success('发送成功');
@@ -41,6 +43,25 @@ class Receipt extends Controller
             $status = db("receipt")->where("id",1)->field("status")->find();
             if(!empty($status)){
                 return ajax_success('发送成功',$status);
+            } else {
+                return ajax_error("发送失败");
+            }
+        }
+
+    }
+
+    /**
+     * [发票添加个人新户名]
+     * 郭杨
+     */
+    public function people(Request $request){
+        if($request->isPost()){
+            $time = time();
+            $data = $request->param();
+            $data["create_time"] = $time;
+            if(!empty($data)){
+                $bool = db("member_receipt")->insert($data);
+                return ajax_success('发送成功');
             } else {
                 return ajax_error("发送失败");
             }
