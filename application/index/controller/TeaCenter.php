0e07e9a51a1ac8901030f6eeca40aa52a1c4da17
guoyangzt <gy156789@163.com>
2019-04-16 16:08:50 +0800

    拉代码

--- application/index/controller/TeaCenter.php
+++ application/index/controller/TeaCenter.php
@@ -125,8 +125,8 @@ class TeaCenter extends Controller
                 'classify_image'=>$actdata['classify_image'],
                 'address'=>$actdata['address'],
                 'cost_moneny'=>$actdata['cost_moneny'],
-                'start_time'=>date('Y-m-d',$actdata['start_time']),
-                'end_time'=>date('Y-m-d ',$actdata['end_time']),
+                'start_time'=>$actdata['start_time'],
+                'end_time'=>$actdata['end_time'],
                 'day_start_time'=>$actdata['day_start_time'],
                 'day_end_time'=>$actdata['day_end_time'],
                 'participats'=>$actdata['participats'],
@@ -229,15 +229,18 @@ class TeaCenter extends Controller
             $user_id =Db::name("member")->where("member_openid",$open_id)->value("member_id");
             $data = db("teahost")->where('id',$activity_id)->field("activity_name,classify_image,address,pid,cost_moneny,start_time,peoples")->find();
             $account = db("member")->where('member_openid',$open_id)->value('member_phone_num');
+            $names = db("goods_type")->where("id",$data['pid'])->value("name");
 
             $time=date("Y-m-d",time());
             $v=explode('-',$time);
             $time_second=date("H:i:s",time());
             $vs=explode(':',$time_second);
-//            $parts_order_number =$v[0].$v[1].$v[2].$vs[0].$vs[1].$vs[2].rand(1000,9999).($user_id+100000); //订单编号
-            $parts_order_number =$user_id + intval($v[0].$v[1].$v[2].$vs[0].$vs[1].$vs[2]); //订单编号
+            $parts_order_number ="HD".($user_id + intval($v[0].$v[1].$v[2].$vs[0].$vs[1].$vs[2])); //订单编号
             $data['member_openid'] =  $open_id;
+            $data['teahost_id'] =  $activity_id;
             $data['account'] =  $account;
+            $data['status'] =  1;
+            $data['names'] =  $names;
             $data['parts_order_number'] =  $parts_order_number;
             $bool = db("activity_order")->insert($data);
             if (!empty($bool)) {
