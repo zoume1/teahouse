ec3bed09b56b09e57c2ced45bedc15ecf8c81b66
lihuosheng-top <1061525487@qq.com>
2019-04-23 14:14:24 +0800

    资金管理

--- application/route.php
+++ application/route.php
@@ -787,6 +787,7 @@ Route::group("admin",[
     "unline_withdrawal_record"=>"admin/General/unline_withdrawal_record",//资金管理提现记录
     "agency_invitation"=>"admin/General/agency_invitation",//代理分销邀请
     "now_agency_invitation"=>"admin/General/now_agency_invitation",//立即分销邀请
+    "security_setting"=>"admin/General/security_setting",//立即分销邀请
     "store_update_password"=>"admin/Store/store_update_password",//店铺的支付密码修改
     "store_wallet_add"=>"admin/Store/store_wallet_add",//店铺的钱包充值
     "store_wallet_return"=>"admin/Store/store_wallet_return",//店铺的钱包返回
