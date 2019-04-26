bf96f0ef2812ae3fe504631240d02f5c91c47ab5
lihuosheng-top <1061525487@qq.com>
2019-04-26 17:16:00 +0800

    xiaocheng

--- application/index/controller/AdminWx.php
+++ application/index/controller/AdminWx.php
@@ -32,20 +32,51 @@ class  AdminWx extends Controller{
                 $enter_all_id =Db::name("set_meal_order")
                     ->where("order_number",$val["out_trade_no"])
                     ->value("enter_all_id");
+                $enter_all_data=Db::name("set_meal_order")
+                    ->where("order_number",$val["out_trade_no"])
+                    ->find();
                 $year =Db::name("enter_all")->where("id",$enter_all_id)->value("year");
                 //进行逻辑处理
-                $data["pay_time"] =time();//支付时间
-                $data["pay_type"] =1;//支付类型（1扫码支付，2汇款支付，3余额支付）
-                $data["pay_status"] =1;//到账状态（1为已到账，-1未到账，2待审核）
-                $data["pay_status"] =1;//订单审核状态（1审核通过，-1审核不通过,0待审核）
-                $data["start_time"] =time();//开始时间
-                $data["end_time"] =strtotime("+$year  year");//开始时间
-                $data["explains"] ="扫码支付直接通过";//审核说明
-                $data["status"] =1; //订单状态（-1为未付款，1已付款）
-                $data["audit_status"] =1; //订单审核状态（1审核通过，-1审核不通过,0待审核）
-                $result =Db::name("set_meal_order")
-                    ->where("order_number",$val["out_trade_no"])
-                    ->update($data);
+                //1、先判断是否上一单是否到期和是否存在
+                //2、判断如果是升级过来的话需要进行删除之前已付款的订单
+                $is_set_order =Db::name("set_meal_order")
+                    ->where("store",$enter_all_data["store_id"])
+                    ->where("audit_status",1)
+                    ->where("end_time","<",time())
+                    ->find();
+                if($is_set_order){
+                    //这是套餐升级的情况
+                    $data["pay_time"] =time();//支付时间
+                    $data["pay_type"] =1;//支付类型（1扫码支付,2汇款支付，3余额支付）
+                    $data["pay_status"] =1;//到账状态（1为已到账，-1未到账，2待审核）
+                    $data["start_time"] =time();//开始时间
+                    $data["end_time"] =strtotime("+$year  year");//开始时间
+                    $data["explains"] ="扫码支付直接通过";//审核说明
+                    $data["status"] =1; //订单状态（-1为未付款，1已付款）
+                    $data["audit_status"] =1; //订单审核状态（1审核通过，-1审核不通过,0待审核）
+                    $res =Db::name("set_meal_order")
+                        ->where("order_number",$val["out_trade_no"])
+                        ->update($data);
+                    if($res){
+                        //把之前的套餐订单删掉
+                       $result = Db::name("set_meal_order")->where("order_number",$is_set_order["$is_set_order"])->delete();
+                    }else{
+                        $result =0;
+                    }
+                }else{
+                    //这是新加入套餐的情况
+                    $data["pay_time"] =time();//支付时间
+                    $data["pay_type"] =1;//支付类型（1扫码支付，2汇款支付，3余额支付）
+                    $data["pay_status"] =1;//到账状态（1为已到账，-1未到账，2待审核）
+                    $data["start_time"] =time();//开始时间
+                    $data["end_time"] =strtotime("+$year  year");//开始时间
+                    $data["explains"] ="扫码支付直接通过";//审核说明
+                    $data["status"] =1; //订单状态（-1为未付款，1已付款）
+                    $data["audit_status"] =1; //订单审核状态（1审核通过，-1审核不通过,0待审核）
+                    $result =Db::name("set_meal_order")
+                        ->where("order_number",$val["out_trade_no"])
+                        ->update($data);
+                }
                 if($result){
                     echo '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
                 }else{
