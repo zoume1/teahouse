f7150749c69509813e0662cd093fde1526fb71b8
lihuosheng-top <1061525487@qq.com>
2019-04-28 14:51:05 +0800

    支付宝扫码

--- application/index/controller/AdminWx.php
+++ application/index/controller/AdminWx.php
@@ -103,11 +103,81 @@ class  AdminWx extends Controller{
         $obj_alipay = new \alipay();
         if (!$obj_alipay->verify_notify()) {
             //验证未通过
-            file_put_contents(EXTEND_PATH."data121212.txt",1111);
             echo "fail";
             exit();
         }else{
-            file_put_contents(EXTEND_PATH."data121212.txt",222);
+            //这里可以做一下你自己的订单逻辑处理
+            $pay_time = time();
+            $data['pay_time'] = $pay_time;
+            //原始订单号
+            $out_trade_no = input('out_trade_no');
+            //支付宝交易号
+            $trade_no = input('trade_no');
+            //交易状态
+            $trade_status = input('trade_status');
+            if ($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
+                $condition['order_number'] = $out_trade_no;
+                $enter_all_data=Db::name("set_meal_order")
+                    ->where($condition)
+                    ->find();
+                $year =Db::name("enter_all")->where("id",$enter_all_data['enter_all_id'])->value("year");
+                //进行逻辑处理
+                //1、先判断是否上一单是否到期和是否存在
+                //2、判断如果是升级过来的话需要进行删除之前已付款的订单
+                $is_set_order =Db::name("set_meal_order")
+                    ->where("store_id",$enter_all_data["store_id"])
+                    ->where("audit_status",1)
+                    ->find();
+                if($is_set_order){
+                    //这是套餐升级的情况
+                    $data["pay_time"] =time();//支付时间
+                    $data["pay_type"] =1;//支付类型（1扫码支付,2汇款支付，3余额支付）
+                    $data["pay_status"] =1;//到账状态（1为已到账，-1未到账，2待审核）
+                    $data["start_time"] =time();//开始时间
+                    $data["end_time"] =strtotime("+$year  year");//开始时间
+                    $data["explains"] ="支付宝扫码支付直接通过";//审核说明
+                    $data["status"] =1; //订单状态（-1为未付款，1已付款）
+                    $data["audit_status"] =1; //订单审核状态（1审核通过，-1审核不通过,0待审核）
+                    $res =Db::name("set_meal_order")
+                        ->where($condition)
+                        ->update($data);
+                    if($res){
+                        //把之前的套餐订单删掉
+                        $result = Db::name("set_meal_order")
+                            ->where("order_number",$is_set_order["order_number"])
+                            ->delete();
+                        if($result){
+                            return "success";
+                        }else{
+                            return "fail";
+                        }
+                    }else{
+                        $result =0;
+                        if($result){
+                            return "success";
+                        }else{
+                            return "fail";
+                        }
+                    }
+                }else{
+                    //这是新加入套餐的情况
+                    $data["pay_time"] =time();//支付时间
+                    $data["pay_type"] =1;//支付类型（1扫码支付，2汇款支付，3余额支付）
+                    $data["pay_status"] =1;//到账状态（1为已到账，-1未到账，2待审核）
+                    $data["start_time"] =time();//开始时间
+                    $data["end_time"] =strtotime("+$year  year");//开始时间
+                    $data["explains"] ="支付宝扫码支付直接通过";//审核说明
+                    $data["status"] =1; //订单状态（-1为未付款，1已付款）
+                    $data["audit_status"] =1; //订单审核状态（1审核通过，-1审核不通过,0待审核）
+                    $result =Db::name("set_meal_order")
+                        ->where($condition)
+                        ->update($data);
+                    if($result){
+                        return "success";
+                    }else{
+                        return "fail";
+                    }
+            }
         }
 
     }
