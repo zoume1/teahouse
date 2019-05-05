599aa2942ae34e498ad67a038b0339fe3d57dd61
zoume1 <595649828@qq.com>
2019-05-05 17:13:30 +0800

    ppuulp

--- application/index/controller/AdminWx.php
+++ application/index/controller/AdminWx.php
@@ -77,12 +77,14 @@ class  AdminWx extends Controller{
                     $data["explains"] ="微信扫码支付直接通过";//审核说明
                     $data["status"] =1; //订单状态（-1为未付款，1已付款）
                     $data["audit_status"] =1; //订单审核状态（1审核通过，-1审核不通过,0待审核）
+
                     $result =Db::name("set_meal_order")
                         ->where("order_number",$val["out_trade_no"])
                         ->update($data);
-                    $whe['pay_type'] = array('neq',1);
-                     Db::name("set_meal_order")->where($whe)->delete();
+                   
                     if($result){
+                         $whe['pay_type'] = array('neq',1);
+                         Db::name("set_meal_order")->where($whe)->delete();
                         //审核通过则对店铺进行开放，修改店铺的权限（普通访客）为商家店铺
                         Db::table("tb_admin")
                             ->where("store_id",$enter_all_data["store_id"])
