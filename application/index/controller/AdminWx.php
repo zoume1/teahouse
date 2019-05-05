ce43702befceba86073eab2190bd05d6e7dab73c
zoume1 <595649828@qq.com>
2019-05-05 15:52:59 +0800

    llppppppo

--- application/index/controller/AdminWx.php
+++ application/index/controller/AdminWx.php
@@ -53,6 +53,7 @@ class  AdminWx extends Controller{
                         ->update($data);
                     if($res){
                         //把新生成的套餐订单删掉
+                        // Db::name("set_meal_order")->where("order_number",$val["out_trade_no"])->delete(); 火生
                         Db::name("set_meal_order")->where("order_number",$val["out_trade_no"])->update($data);
                         //审核通过则对店铺进行开放，修改店铺的权限（普通访客）为商家店铺
                         Db::table("tb_admin")
@@ -209,9 +210,8 @@ class  AdminWx extends Controller{
                         ->update($data);
                     if ($res){
                         //把刚下套餐订单删掉
-                        $result = Db::name("set_meal_order")
-                            ->where($condition)
-                            ->delete();
+                       // $result = Db::name("set_meal_order")->where($condition)->delete();火生
+                        $result = Db::name("set_meal_order")->where($condition)->update($data);
                         if ($result) {
                             //进行角色转化
                             //审核通过则对店铺进行开放，修改店铺的权限（普通访客）为商家店铺
