36210a8eb428efb1eebb9e495165a705d828c9ff
guoyangzt <gy156789@163.com>
2019-06-24 14:50:40 +0800

    拉代码

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -26,8 +26,14 @@ class  Order extends  Controller
     {
         if ($request->isPost()) {
             $open_id =$request->only("open_id")["open_id"];
-            $member_grade_id =Db::name("member")->where("member_openid",$open_id)->find();
-            $member_consumption_discount =Db::name("member_grade")
+            $member_grade_id = Db::name("member")->where("member_openid",$open_id)->find();
+            $role_id =  Db::name("admin")->where("store_id",$member_grade_id['store_id'])->value("role_id");
+            if($role_id > 13){
+                $authority = 1;
+            } else {
+                $authority = 0;
+            }
+            $member_consumption_discount = Db::name("member_grade")
                 ->where("member_grade_id",$member_grade_id["member_grade_id"])
                 ->find();
             $goods_id = $request->only("goods_id")["goods_id"];
@@ -36,6 +42,7 @@ class  Order extends  Controller
             if (empty($goods_id)) {
                 return ajax_error("商品信息有误，请返回重新提交", ["status" => 0]);
             }
+            
             foreach ($goods_id as  $key=>$value){
                 $goods_data = null;
                 $goods_data = Db::name("goods")->where("id", $value)->find();
@@ -67,6 +74,7 @@ class  Order extends  Controller
                 }
             }
             if(!empty($data)){
+                $data['$authority'] = $$authority;
                 return ajax_success("数据返回",$data);
             }else{
                 return ajax_error("没有数据",["status"=>0]);
