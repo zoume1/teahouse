a84e156451c29067b31c14dbb476c047bfb64865
sunshinefan <604290342@qq.com>
2019-06-25 14:07:15 +0800

    pull

--- application/api/controller/Wxapps.php
+++ application/api/controller/Wxapps.php
@@ -330,7 +330,16 @@ class  Wxapps extends  Controller{
                                         $list2[$kk]['linktype'] = "page";
                                         $jianjie=json_decode($vv['limit_condition'],true);
                                         $list2[$kk]['goods_selling']=$jianjie['label']['label'];
-                                        $list2[$kk]['endtime']=date('H:i:s',$vv['end_time']-time());
+                                        $list2[$kk]['endtime']=$vv['end_time']-time();
+                                        $list2[$kk]['end_time']=$vv['end_time']-time();
+                                        if($list2[$kk]['end_time']<0)    //已结束
+                                        {
+                                          $list2[$kk]['t_flag']=2;
+                                        }elseif($vv['create_time']>time()){   //活动未开始
+                                           $list2[$kk]['t_flag']=1;
+                                        }else{
+                                            $list2[$kk]['t_flag']=0;
+                                        }
                                         $goods_images='//uploads/'.$vv['goods_show_images'];
                                         // $list[$kk]['sale_num'] = $vv['sale_num'] + $vv['sale_tnum'];
                                         if (strpos($goods_images, 'http') === false && $goods_images != "") {
