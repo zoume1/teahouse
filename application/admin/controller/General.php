054ea709c023cd25be26ba4da321ea7b2c865474
guoyangzt <gy156789@163.com>
2019-07-03 18:11:01 +0800

    提交代码

--- application/admin/controller/General.php
+++ application/admin/controller/General.php
@@ -1529,11 +1529,12 @@ class  General extends  Base {
     public function order_package_buy(Request $request){
         if($request->isPost()){
             $id = $request->only(["id"])["id"];
-            $data =Db::table("tb_meal_orders")
+            $data = Db::table("tb_meal_orders")
                  ->alias('a')
                  ->join('tb_enter_all b','a.enter_all_id=b.id','left')
+                 ->join('tb_store c','c.id = b.id','left')
                 ->field("a.id,a.order_number,a.create_time,a.goods_name,a.goods_quantity,
-                    a.amount_money,a.store_id,a.images_url,a.store_name,a.unit,a.cost,a.enter_all_id,b.year")
+                    a.amount_money,a.store_id,a.images_url,a.store_name,a.unit,a.cost,a.enter_all_id,b.year,c.highe_share_code")
                 ->where("a.store_id",$this->store_ids)
                 ->where("a.status",-1)
                 ->where("a.id",$id)
@@ -2940,8 +2941,10 @@ class  General extends  Base {
                         // 更新商家店铺上一级店铺id
                         if(!empty($number)){
                             $bool = db("store")->where("id",$this->store_ids)->update(["share_store_id"=>$number["id"]]);
+                            $bool = db("store")->where("id",$this->store_ids)->update(["highe_share_code"=>$code]);
                         } else {
                             $rest = db("store")->where("id",$this->store_ids)->update(["share_store_id"=>$share_code["id"]]);
+                            $rest = db("store")->where("id",$this->store_ids)->update(["highe_share_code"=>$code]);
                         }
                         return ajax_success("分享码正确");
                     }
