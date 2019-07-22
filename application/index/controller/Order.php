07dc6d8cdd37f5297b2f1575dda72eacc2da762e
guoyangzt <gy156789@163.com>
2019-07-22 18:07:06 +0800

    拉代码

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -2163,8 +2163,8 @@ class  Order extends  Controller
                     $boolw = Db::name('special') -> where('id',$goods_order[$k]['special_id']) ->setInc('volume',$goods_order[$k]['order_quantity']);
                     $booles = Db::name('special') -> where('id',$goods_order[$k]['special_id']) ->setDec('stock',$goods_order[$k]['order_quantity']);
                 } else {
-                    $boolw = Db::name('goods') -> where('id',$goods_order[$k]['goods_id']) ->setInc('goods_volume',$goods_order[$k]['order_quantity']);
-                    $booles = Db::name('goods') -> where('id',$goods_order[$k]['goods_id']) ->setDec('goods_repertory',$goods_order[$k]['order_quantity']);
+                    $boolwtt = Db::name('goods') -> where('id',$goods_order[$k]['goods_id']) ->setInc('goods_volume',$goods_order[$k]['order_quantity']);
+                    $booltt = Db::name('goods') -> where('id',$goods_order[$k]['goods_id']) ->setDec('goods_repertory',$goods_order[$k]['order_quantity']);
                 }
             }
 
