7b55c9cc74327df99f27d6ef1423f80ea8d87b68
sunshinefan <604290342@qq.com>
2019-07-25 11:46:21 +0800

    pull

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -30,11 +30,23 @@ class  Order extends  Controller{
             ->order("order_create_time","desc")
             ->where("store_id",'EQ',$store_id)
             ->where($where)
-            ->page('parts_order_number')
+            ->group('parts_order_number')
             ->paginate(20 ,false, [
                 'query' => request()->param(),
             ]);
-
+halt($data);
+            foreach($data['data'] as $k=>$v){
+                //获取相同订单的数据
+                $list=db('order')->where('parts_order_number',$v['parts_order_number'])->select();
+                $order=[];
+                foreach($list as $k2 =>$v2){
+                    $order[$k2]['goods_image']=$v2['goods_image'];
+                    $order[$k2]['parts_goods_name']=$v2['parts_goods_name'];
+                    $order[$k2]['order_quantity']=$v2['order_quantity'];
+                }
+                $data['data'][$k]['order']=$order;
+            }
+         halt($data);
         return view("order_index",["data"=>$data]);
     }
 
