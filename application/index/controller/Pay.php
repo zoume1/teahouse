28e3a7c99e3ef0d92c0a4814c8a419a08c29b23c
sunshinefan <604290342@qq.com>
2019-07-25 10:15:11 +0800

    pull

--- application/index/controller/Pay.php
+++ application/index/controller/Pay.php
@@ -249,6 +249,7 @@ class Pay extends  Controller{
         //$input->SetOpenid($this->getSession()->openid);
         //         向微信统一下单，并返回order，它是一个array数组
         $order = \WxPayApi::unifiedOrder($input);
+        halt($order);
         //       json化返回给小程序端
         header("Content-Type: application/json");
         echo $this->getJsApiParameters($order);
