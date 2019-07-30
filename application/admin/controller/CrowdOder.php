15b1b24647e5549762ded6967ac5bdae4aa1c790
guoyangzt <gy156789@163.com>
2019-07-30 17:35:45 +0800

    拉代码

--- application/admin/controller/CrowdOder.php
+++ application/admin/controller/CrowdOder.php
@@ -78,9 +78,10 @@ class CrowdOder extends Controller{
         if($request->isPost()){
             $order_id = $request->only(["order_id"])["order_id"];
             if(!empty($order_id)){
-                $data =Db::name("crowd_order")->where("id",$order_id)->find();
+                $data = Db::name("crowd_order")->where("id",$order_id)->find();
                 if(!empty($data)){
                     $data["member_name"] = Db::name("member")->where("member_id",$data["member_id"])->value("member_name");
+                    $data["store_name"] = Db::name("store")->where("id",$data["store_id"])->value("store_name");
                     $data["goods_franking"] = Db::name("crowd_goods")->where("id",$data["goods_id"])->value("goods_franking");
                     return ajax_success("数据返回成功",$data);
                 }else{
