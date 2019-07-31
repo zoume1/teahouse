0ff5d493236bef0effbe5a8361c633ddc5e75a97
lilu1233 <lilusiring@163.com>
2019-07-31 14:29:40 +0800

    push lilu

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -91,11 +91,11 @@ class  Order extends  Controller{
         if($request->isPost()){
             $order_id =$request->only(["order_id"])["order_id"];
             if(!empty($order_id)){
-                $data =Db::name("order")->where("parts_order_number",$order_id)->find();
+                $data =Db::name("order")->where("id",$order_id)->find();
                 if(!empty($data)){
                     $data['store_name'] = db("store")->where("id",$data['store_id'])->value('store_name');
-                    $data['parts_goods_name'] = Db::name("order")->where("parts_order_number",$order_id)->field('parts_goods_name')->select();
-                    $data['order_quantity'] = Db::name("order")->where("parts_order_number",$order_id)->field('order_quantity')->select();
+                    $data['parts_goods_name'] = Db::name("order")->where("id",$order_id)->field('parts_goods_name')->select();
+                    $data['order_quantity'] = Db::name("order")->where("id",$order_id)->field('order_quantity')->select();
                     $data["goods_franking"] = Db::name("goods")->where("id",$data["goods_id"])->value("goods_franking");
                     return ajax_success("数据返回成功",$data);
                 }else{
