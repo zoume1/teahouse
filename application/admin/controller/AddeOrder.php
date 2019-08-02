4d433511bdca61278ebef6799730b59d61f9d5b8
guoyangzt <gy156789@163.com>
2019-08-02 09:53:52 +0800

    拉代码

--- application/admin/controller/AddeOrder.php
+++ application/admin/controller/AddeOrder.php
@@ -149,34 +149,27 @@ class  AddeOrder extends  Controller{
                 ];
             }
 
-
-
             $booles = Db::name('adder_order')->where('parts_order_number',$order_number)->update($datas);
-            if($booles){
-                $notify = new \NativePay();
-                $input = new \WxPayUnifiedOrder();//统一下单
-                $goods_id = 123456789; //商品Id
-                $input->SetBody($goods_name);//设置商品或支付单简要描述
-                $input->SetAttach($goods_name);//设置附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
-                $input->SetOut_trade_no($order_number);//设置商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
-                $input->SetTotal_fee($order_real_pay * 100);//金额乘以100
-                $input->SetTime_start(date("YmdHis")); //设置订单生成时间,格式为yyyyMMddHHmmss
-                $input->SetTime_expire(date("YmdHis", time() + 600)); //设置订单失效时间
-                $input->SetGoods_tag("test"); //设置商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
-                $input->SetNotify_url(config("domain.url")."/analyse_meal_notify"); //回调地址
-                $input->SetTrade_type("NATIVE"); //交易类型(扫码)
-                $input->SetProduct_id($goods_id);//设置trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义。
-                $result = $notify->GetPayUrl($input);
-                $url2 = $result["code_url"];
-                if($url2){
-                    return ajax_success("微信二维码返回成功",["url"=>"/qrcode?url2=".$url2]);
-                }else{
-                    return ajax_error("二维码生成失败");
-                }
-            } else {
-                return ajax_error("订单有误,请稍后再试");
+            $notify = new \NativePay();
+            $input = new \WxPayUnifiedOrder();//统一下单
+            $goods_id = 123456789; //商品Id
+            $input->SetBody($goods_name);//设置商品或支付单简要描述
+            $input->SetAttach($goods_name);//设置附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
+            $input->SetOut_trade_no($order_number);//设置商户系统内部的订单号,32个字符内、可包含字母, 其他说明见商户订单号
+            $input->SetTotal_fee($order_real_pay * 100);//金额乘以100
+            $input->SetTime_start(date("YmdHis")); //设置订单生成时间,格式为yyyyMMddHHmmss
+            $input->SetTime_expire(date("YmdHis", time() + 600)); //设置订单失效时间
+            $input->SetGoods_tag("test"); //设置商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
+            $input->SetNotify_url(config("domain.url")."/analyse_meal_notify"); //回调地址
+            $input->SetTrade_type("NATIVE"); //交易类型(扫码)
+            $input->SetProduct_id($goods_id);//设置trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义。
+            $result = $notify->GetPayUrl($input);
+            $url2 = $result["code_url"];
+            if($url2){
+                return ajax_success("微信二维码返回成功",["url"=>"/qrcode?url2=".$url2]);
+            }else{
+                return ajax_error("二维码生成失败");
             }
-
         }
     }
 
