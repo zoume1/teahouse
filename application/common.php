e77cd462e7c991819a8227567cb9bf9eeda5fc5e
Zheng-jp <970663468@qq.com>
2019-08-03 16:49:38 +0800

    pull002

--- application/common.php
+++ application/common.php
@@ -741,17 +741,17 @@ function show_order_statues($status){
     }else if($status==1){
         echo '<button type="button" class="state  payment-has-been" >已付款</button>';
     }else  if($status==2){
-        echo '<button type="button" class="state  shipmenting-btu" >待发货</button>';
+        echo '<button type="button" class="state  shipmenting-btu btn-dfh" >待发货</button>';
     }else  if($status==3){
-        echo '<button type="button" class="state  shipmented-btu" >已发货</button>';
+        echo '<button type="button" class="state  shipmented-btu btn-yfh" >已发货</button>';
     }else  if($status==4){
-        echo '<button type="button" class="state  gooding-btu static_btus" >待收货</button>';
+        echo '<button type="button" class="state  gooding-btu static_btus btn-dfk" >待收货</button>';
     }else  if($status==5){
         echo '<button type="button" class="state  gooded-btu" >已收货</button>';
     }else  if($status==7){
         echo '<button type="button" class="state  obligation" >待评价</button>';
     } else  if($status==8){
-        echo '<button type="button" class="state  finish-btu" >已完成</button>';
+        echo '<button type="button" class="state  finish-btu btn-ywc" >已完成</button>';
     }else  if($status==9){
         echo '<button type="button" class="state  cancel-btu" >取消订单</button>';
     }else  if($status==10){
@@ -759,7 +759,7 @@ function show_order_statues($status){
     }else  if($status==11){
         echo '<button type="button" class="state  cancel-btu" >退货</button>';
     }else  if($status==12){
-        echo '<button type="button" class="state  cancel-btu static_btus" >待服务</button>';
+        echo '<button type="button" class="state  cancel-btu static_btus btn-ytk" >待服务</button>';
     }
 }
 
