0177b6835a45111d11cc48835677dd5ffef12404
guoyangzt <gy156789@163.com>
2019-08-03 15:03:39 +0800

    回复处理

--- application/admin/controller/AddeOrder.php
+++ application/admin/controller/AddeOrder.php
@@ -141,13 +141,31 @@ class  AddeOrder extends  Controller{
                     ];
                 }
             } else {
-                $datas = [
-                    'order_real_pay' => $order_real_pay,
-                    'order_amount' => $order_amount,
-                    'order_quantity' => $order_quantity,
-                    // 'coupon_deductible' => $coupon_deductible,
-                    'freight'=>0
-                ];
+                $is_address_status = Db::name("pc_store_address")->where('store_id',$store_id)->where('default',1)->find(); //默认收货地址详细
+                if(!empty($is_address_status)){
+                    $harvest_address_city = str_replace(' ','',$is_address_status['address']);
+                    $harvest_address = $harvest_address_city.$is_address_status['street']; //收货人地址
+                    $harvester = $is_address_status['name'];
+                    $harvester_phone_num = $is_address_status['phone'];
+                    $datas = [
+                        'order_real_pay' => $order_real_pay,
+                        'order_amount' => $order_amount,
+                        'order_quantity' => $order_quantity,
+                        'harvester' => $harvester,
+                        'harvest_phone_num' => $harvester_phone_num,
+                        'harvester_address' => $harvest_address,
+                        // 'coupon_deductible' => $coupon_deductible,
+                        'freight'=>0
+                    ];
+                } else {
+                    $datas = [
+                        'order_real_pay' => $order_real_pay,
+                        'order_amount' => $order_amount,
+                        'order_quantity' => $order_quantity,
+                        // 'coupon_deductible' => $coupon_deductible,
+                        'freight'=>0
+                    ];
+                }
             }
 
             $booles = Db::name('adder_order')->where('parts_order_number',$order_number)->update($datas);
@@ -217,13 +235,31 @@ class  AddeOrder extends  Controller{
                     ];
                 }
             } else {
-                $datas = [
-                    'order_real_pay' => $order_real_pay,
-                    'order_amount' => $order_amount,
-                    'order_quantity' => $order_quantity,
-                    // 'coupon_deductible' => $coupon_deductible,
-                    'freight'=>0
-                ];
+                $is_address_status = Db::name("pc_store_address")->where('store_id',$store_id)->where('default',1)->find(); //默认收货地址详细
+                if(!empty($is_address_status)){
+                    $harvest_address_city = str_replace(' ','',$is_address_status['address']);
+                    $harvest_address = $harvest_address_city.$is_address_status['street']; //收货人地址
+                    $harvester = $is_address_status['name'];
+                    $harvester_phone_num = $is_address_status['phone'];
+                    $datas = [
+                        'order_real_pay' => $order_real_pay,
+                        'order_amount' => $order_amount,
+                        'order_quantity' => $order_quantity,
+                        'harvester' => $harvester,
+                        'harvest_phone_num' => $harvester_phone_num,
+                        'harvester_address' => $harvest_address,
+                        // 'coupon_deductible' => $coupon_deductible,
+                        'freight'=>0
+                    ];
+                } else {
+                    $datas = [
+                        'order_real_pay' => $order_real_pay,
+                        'order_amount' => $order_amount,
+                        'order_quantity' => $order_quantity,
+                        // 'coupon_deductible' => $coupon_deductible,
+                        'freight'=>0
+                    ];
+                }
             }
 
             $booles = Db::name('adder_order')->where('parts_order_number',$order_number)->update($datas);
@@ -310,13 +346,31 @@ class  AddeOrder extends  Controller{
                     ];
                 }
             } else {
-                $datas = [
-                    'order_real_pay' => $order_real_pay,
-                    'order_amount' => $order_amount,
-                    'order_quantity' => $order_quantity,
-                    // 'coupon_deductible' => $coupon_deductible,
-                    'freight'=>0
-                ];
+                $is_address_status = Db::name("pc_store_address")->where('store_id',$store_id)->where('default',1)->find(); //默认收货地址详细
+                if(!empty($is_address_status)){
+                    $harvest_address_city = str_replace(' ','',$is_address_status['address']);
+                    $harvest_address = $harvest_address_city.$is_address_status['street']; //收货人地址
+                    $harvester = $is_address_status['name'];
+                    $harvester_phone_num = $is_address_status['phone'];
+                    $datas = [
+                        'order_real_pay' => $order_real_pay,
+                        'order_amount' => $order_amount,
+                        'order_quantity' => $order_quantity,
+                        'harvester' => $harvester,
+                        'harvest_phone_num' => $harvester_phone_num,
+                        'harvester_address' => $harvest_address,
+                        // 'coupon_deductible' => $coupon_deductible,
+                        'freight'=>0
+                    ];
+                } else {
+                    $datas = [
+                        'order_real_pay' => $order_real_pay,
+                        'order_amount' => $order_amount,
+                        'order_quantity' => $order_quantity,
+                        // 'coupon_deductible' => $coupon_deductible,
+                        'freight'=>0
+                    ];
+                }
             }
             $goods_type = Db::name("adder_order")->where('parts_order_number',$order_number)->value('goods_type');
     
