7cad336ffa1e7c3381161d14a1091df38d764e20
Zheng-jp <970663468@qq.com>
2019-08-08 15:22:29 +0800

    pull002

--- application/common.php
+++ application/common.php
@@ -1295,7 +1295,7 @@ function show_house_orderer($status){
         echo '<button type="button" class="layui-btn layui-btn-mini color1" >待付款</button>';
         //暂时显示到入仓状态
     }else  if($status==2){
-        echo '<button type="button" class="layui-btn layui-btn-mini enter-btu" >待发货</button>';
+        echo '<button type="button" class="layui-btn layui-btn-mini color2" >待发货</button>';
     }else  if($status==3){
         echo '<button type="button" class="layui-btn layui-btn-mini close-btu" >已入仓</button>';
     }
