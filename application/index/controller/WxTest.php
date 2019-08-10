88460316b5c10f4a02dda647a9f46c42a67aa5fa
lilu1233 <lilusiring@163.com>
2019-08-10 16:48:15 +0800

    pull

--- application/index/controller/WxTest.php
+++ application/index/controller/WxTest.php
@@ -194,10 +194,11 @@ class WxTest extends Controller
          */
         public function callback(){
         // 每个授权小程序的appid，在第三方平台的消息与事件接收URL中设置了 $APPID$ 
-        echo '212';
         $authorizer_appid = input('param.appid/s'); 
         // 每个授权小程序传来的加密消息
         $postStr = file_get_contents("php://input");
+        dump($authorizer_appid);
+        halt($postStr);
         if (!empty($postStr)){
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
  
