4b9f3b3f4aa00454d298362d880883fb0c652d4f
guoyangzt <gy156789@163.com>
2019-08-17 15:01:21 +0800

    拉代码

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -398,7 +398,8 @@ class  Order extends  Controller
                                 'status'=>0,
                                 
                             ];
-                        }                                               
+                        }        
+                            halt($commind_data);                                     
                             OrderModel::createOrder($commind_data);
                             return ajax_success('下单成功',$order_datas);
                         }else{
