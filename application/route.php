938be41f291f91c77d9154c2570cd10b4a95ded4
lilu1233 <lilusiring@163.com>
2019-08-19 15:06:42 +0800

    push

--- application/route.php
+++ application/route.php
@@ -418,6 +418,7 @@ Route::group("admin",[
     "shop_store_date"=>"admin/index/shop_store_date",         //进入店铺后台显示但前使用版本及日期
     "get_info_store"=>"admin/index/get_info_store",           //店铺后台获取消息
     "get_info_zong"=>"admin/index/get_info_zong",             //总控后台获取消息
+    "informationhint"=>"admin/index/informationhint",         //总控后台判断消息提醒
 
     /* 后台首页 */
     "home_index"=>"admin/Home/index",
