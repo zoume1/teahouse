b30aa71396f8b8c5cb34a285f8af22d87f594fe7
lilu1233 <lilusiring@163.com>
2019-08-21 17:59:52 +0800

    pull

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -720,7 +720,7 @@ class Upload extends Controller
             header('Last-Modified: ' . $_SERVER['REQUEST_TIME']);
             header('Content-Type: image/jpeg');
             //这就是1张图 Content-Type: image/jpeg 
-            echo file_get_contents($url);
+            echo  '<img src="data:'.file_get_contents($url).'">';
                 // return ajax_success('获取成功',["url"=>$url]);
             }
     }
