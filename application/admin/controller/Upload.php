59c1d65c08720a2dd595b2d3bc2a68a6eae0531f
lilu1233 <lilusiring@163.com>
2019-08-22 15:11:12 +0800

    pull

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -695,12 +695,12 @@ class Upload extends Controller
         //判断access_token是否过期，重新获取
         $store_id=Session::get('store_id');
         $appid=db('miniprogram')->where('store_id',$store_id)->value('appid');
-        // $timeout=$this->is_timeout($appid);
-        //     if($path){
-        //         $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token']."&path=".urlencode($path);
-        //     } else {
-        //         $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token'];
-        //     }
+        $timeout=$this->is_timeout($appid);
+            if($path){
+                $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token']."&path=".urlencode($path);
+            } else {
+                $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token'];
+            }
             $url2='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566456234027&di=aea1f4e7b7a82bb83abac8fcc66a45e4&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201402%2F08%2F20140208125848_mL3Jw.jpeg';
             // $url2="https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token'];
             //这就是1张图 Content-Type: image/jpeg
