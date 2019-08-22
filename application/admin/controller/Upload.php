d3439f8eed5ebd906ba5c50908b17e4041be2c1d
lilu1233 <lilusiring@163.com>
2019-08-22 17:42:31 +0800

    pull

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -376,12 +376,12 @@ class Upload extends Controller
             $qr=new My();
             $qrcode=$qr->mpcode2($page,$store_id);
             //把qrcode文件写进文件中，使用的时候拿出来
-            $new_file = ROOT_PATH . 'public' . DS . 'uploads'.DS.'D6'.'.txt';
+            $new_file = ROOT_PATH . 'public' . DS . 'uploads'.DS.'D6.txt';
                 //检查是否有该文件夹，如果没有就创建，并给予最高权限
                 // mkdir($new_file, 0777);
                 // mkdir($new_file, 750);
             if (file_put_contents($new_file, $qrcode)) {
-                $re=file_get_contents(ROOT_PATH . 'public' . DS . 'uploads'.DS.'D'.$store_id.'.txt');
+                $re=file_get_contents(ROOT_PATH . 'public' . DS . 'uploads'.DS.'D6.txt');
             } 
         // }
         //判断是否已授权
