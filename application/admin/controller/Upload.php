f20c7f36db46861bffaeb42fbcacf0f9385257f6
lilu1233 <lilusiring@163.com>
2019-08-22 14:52:04 +0800

    pull

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -695,22 +695,20 @@ class Upload extends Controller
         //判断access_token是否过期，重新获取
         $store_id=Session::get('store_id');
         $appid=db('miniprogram')->where('store_id',$store_id)->value('appid');
-        $timeout=$this->is_timeout($appid);
-            if($path){
-                $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token']."&path=".urlencode($path);
-            } else {
-                $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token'];
-            }
+        // $timeout=$this->is_timeout($appid);
+        //     if($path){
+        //         $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token']."&path=".urlencode($path);
+        //     } else {
+        //         $url = "https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token'];
+        //     }
             $url2='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566456234027&di=aea1f4e7b7a82bb83abac8fcc66a45e4&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201402%2F08%2F20140208125848_mL3Jw.jpeg';
             // $url2="https://api.weixin.qq.com/wxa/get_qrcode?access_token=".$timeout['authorizer_access_token'];
             //这就是1张图 Content-Type: image/jpeg
             $data=file_get_contents($url2);
             // echo '<img src="data:'.$data.'">';
-            // header('Cache-Control: public');
-            // header('Last-Modified: '.$_SERVER['REQUEST_TIME']);
             header('Content-Type: image/jpeg;');
-            echo '<img src="'.$data.'">';
-            // echo $data;
+            echo $data;
+
             // $ret2 = $this->https_get2($url);
             // $ret = json_decode($ret2,true);
             // $p['msg']=$ret2.'体验码';
@@ -721,20 +719,6 @@ class Upload extends Controller
             //     return ajax_success('获取成功',["url"=>$url]);
             // }
     }
-    // public function serverIp(){
-    //     if(isset($_SERVER)){
-    //         if($_SERVER['SERVER_ADDR']){
-    //             $server_ip=$_SERVER['SERVER_ADDR'];
-    //         }else{
-    //             $server_ip=$_SERVER['LOCAL_ADDR'];
-    //         }
-    //     }else{
-    //         $server_ip = getenv('SERVER_ADDR');
-    //     }
-    //     return $server_ip;
-    // }
-    
-    
     /**
      * lilu
      * 提交审核
