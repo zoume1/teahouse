b7ec1d952d585b11936bddbc48ee8424f1fd0031
lilu1233 <lilusiring@163.com>
2019-08-22 10:09:19 +0800

    pull

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -705,8 +705,8 @@ class Upload extends Controller
             header('Last-Modified: '.$_SERVER['REQUEST_TIME']);
             header('Content-Type: image/jpeg');
             //这就是1张图 Content-Type: image/jpeg
-            echo file_get_contents($url); 
-            // $data=file_get_contents($url);
+            $data=file_get_contents($url);
+            halt($data); 
             // echo '<img src="data:'.$data.'">';
             exit();
             $ret2 = $this->https_get2($url);
