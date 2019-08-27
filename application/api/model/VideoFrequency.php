5990d3ee7db5fa8c66b40fc248e2672bdf7739c9
lilu1233 <lilusiring@163.com>
2019-08-27 15:42:45 +0800

    直播显示接口

--- application/api/model/VideoFrequency.php
+++ application/api/model/VideoFrequency.php
@@ -11,14 +11,37 @@ class VideoFrequency extends Model
     //查询分类直播列表
     public static function live_broadcast($store_id,$lid)
     {
-        return self::all(['store_id'=>$store_id,'classify_id'=>$lid,'status'=>1])
+        $data = self::all(['store_id'=>$store_id,'classify_id'=>$lid,'status'=>1])
          -> toArray();
+
+        foreach ($data as $k =>$v){
+            //调取评论数
+            $comment = new VideoComment();
+            $comment_count = $comment->comment_count($store_id,$v['id']);
+            $data[$k]['comments'] = $comment_count;
+            //调取总点赞数
+            $give = new Give();
+            $gice_count = $give->give_count($store_id,$v['id']);
+            $data[$k]['clickings'] = $gice_count;
+        }
+        return $data;
+
     }
 
     //直播详情
     public static function live_details($store_id,$vid)
     {
-        return self::get(['store_id'=>$store_id,'id'=>$vid]) ->  toArray();
+        $data =  self::get(['store_id'=>$store_id,'id'=>$vid]) ->  toArray();
+        //调取评论数
+        $comment = new VideoComment();
+        $comment_count = $comment->comment_count($store_id,$vid);
+        $data['comments'] = $comment_count;
+        //调取总点赞数
+        $give = new Give();
+        $gice_count = $give->give_count($store_id,$vid);
+        $data['clickings'] = $gice_count;
+
+        return $data;
     }
 
     //直播浏览量+1
