7496052da9a5e7b950707150ad91b9be0e7326e9
lilu1233 <lilusiring@163.com>
2019-09-04 11:08:40 +0800

    pull

--- application/admin/controller/Qiniu.php
+++ application/admin/controller/Qiniu.php
@@ -23,7 +23,7 @@ class Qiniu extends Controller
     public static function uploadimg($accessKey,$secrectkey,$bucket,$domain,$images)
     {
         $file = request()->file($images);
-        if (!empty($file)) {              
+        if (!empty($file) && is_array($file)) {              
             foreach ($file as $k=>$v) {
                 $info = $v->move(ROOT_PATH . 'public' . DS . 'uploads');    //本地保存
                 $filePath = $info->getPathName();
@@ -51,8 +51,33 @@ class Qiniu extends Controller
                 $list[] = 'http://'.$domain.'/'.$ret['key'];
 
             }    
-            // halt($list);    
             return $list;    
+        }elseif(!empty($file)){
+            $info = $fiel->move(ROOT_PATH . 'public' . DS . 'uploads');    //本地保存
+            $filePath = $info->getPathName();
+            // 要上传图片的本地路径
+            $ext = pathinfo($info->getInfo('name'), PATHINFO_EXTENSION);  //后缀
+            //获取当前控制器名称
+            $controllerName = 'index';
+            // 上传到七牛后保存的文件名
+            $key =substr(md5($info->getRealPath()) , 0, 5). date('YmdHis') . rand(0, 9999) . '.' . $ext;
+            // 需要填写你的 Access Key 和 Secret Key
+            // 构建鉴权对象
+            $auth = new Auth($accessKey,$secrectkey);
+            // 要上传的空间
+            $token = $auth->uploadToken($bucket);
+            // 初始化 UploadManager 对象并进行文件的上传
+            $uploadMgr = new UploadManager();
+            // 调用 UploadManager 的 putFile 方法进行文件的上传
+            list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
+            if ($err !== null) {
+                echo ["err"=>1,"msg"=>$err,"data"=>""];
+            } else {
+                //返回图片的完整URL
+                // return $ret[''];
+            }
+            $list[] = 'http://'.$domain.'/'.$ret['key'];
+            return $list;
         }
     }
     //   public static function uploadimg($accessKey,$secrectkey,$bucket,$domain,$images)
