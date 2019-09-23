88cae645a1b3340939733fd292ddbb201b972524
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-09-23 15:17:31 +0800

    优化

---
+++ application/rec/controller/With.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PHP
+ * Date: 2019/9/23
+ * Time: 15:07
+ */
+namespace app\rec\controller;
+use think\Request;
+use think\Validate;
+use think\Controller;
+
+Class With extends Controller{
+
+
+}
