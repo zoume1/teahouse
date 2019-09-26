bb5cd7950497d8942c32a8e5f9435fffdd5d9e8a
lilu1233 <lilusiring@163.com>
2019-09-26 10:28:51 +0800

    pull

--- application/rec/controller/User.php
+++ application/rec/controller/User.php
@@ -133,6 +133,7 @@ class User extends Controller{
     {
         $request = Request::instance();
         $data = $request->param();
+        //print_r($data);die;
         // 验证
         $rules = [
             'phone' => 'require|regex:\d{11}',
@@ -159,7 +160,9 @@ class User extends Controller{
             // 判断密码是否正确
             if (password_verify($data['password'] ,$user['password'])) {
                 //更新openID
-                db('pc_user') ->where('id',$user['id']) ->update(['openid'=>$data['openid']]);
+               if(!empty($data['openid'])){
+                    db('pc_user') ->where('id',$user['id']) ->update(['openid'=>$data['open_id']]);
+                }
                 return json(['code'=>1,'msg'=>'登录成功','user_id'=>$user['id']]);
             }else {
                 return json(['code'=>0,'msg'=>'密码错误']);
