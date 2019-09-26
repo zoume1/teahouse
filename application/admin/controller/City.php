ee7dca899b8f1c8984dee4c609a06b09f033f4fc
guoyangzt <gy156789@163.com>
2019-09-26 15:53:37 +0800

    拉代码

--- application/admin/controller/City.php
+++ application/admin/controller/City.php
@@ -182,8 +182,8 @@ class  City extends  Controller{
      * 郭杨
      */    
     public function city_price_examine_update($id){
-        $order_data =  CityOrder::detail(['id'=>$id]);
-        return view("city_price_examine_update",['order_data'=>$order_data]);
+        $data =  CityOrder::detail(['id'=>$id]);
+        return view("city_price_examine_update",['data'=>$data]);
     }
     /**
      * [城市入驻费用审核]
@@ -202,7 +202,15 @@ class  City extends  Controller{
     public function city_price_examine_replace(Request $request){
         if($request->isPost()){
             $data = input();
-            halt($data);
+            $rest = CityOrder::meal_update($data);
+            $restul  = new CityCopartner;
+            $bool  = $restul->allowField(true)->save(['judge_status'=>$data['account_status']],['user_id'=>$data['city_user_id']]);
+
+            if($rest || $bool){
+                $this->success("更新成功", url("admin/City/city_price_examine"));
+            } else {
+                $this->success("更新成功", url("admin/City/city_price_examine"));
+            }
         }
 
     }
@@ -217,8 +225,8 @@ class  City extends  Controller{
 
     public  function order_preparation($status)
     {
-        $rest = CityOrder::city_order($status);
-        return $rest;
+        $data = CityOrder::order_preparation($status);
+        return view("city_price_examine",['data'=>$data]);
     }
 
 
