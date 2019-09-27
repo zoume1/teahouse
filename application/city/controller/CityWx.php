6e056210292c580ad9069b513db67a8ad15b8561
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-09-27 17:13:47 +0800

    优化

--- application/city/controller/CityWx.php
+++ application/city/controller/CityWx.php
@@ -33,7 +33,7 @@ class CityWx extends Controller{
         //微信公众平台appid
         $appid = $this->sj_appid;
 
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 
         header('Location:'.$url);
 
--- application/city/controller/Passport.php
+++ application/city/controller/Passport.php
@@ -30,7 +30,7 @@ class Passport extends Controller
         if ($this->request->isPost()) {
             $model = new UserModel;
             $code = $model->login($this->postData('User'));
-            
+
             return jsonSuccess($model->getError(),array(),$code);
         }
         
