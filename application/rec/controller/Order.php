9e4b1466b592dedff68df12ec762a2a70a77fadd
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-10-09 18:33:31 +0800

    重新购买订单优化

--- application/rec/controller/Order.php
+++ application/rec/controller/Order.php
@@ -48,8 +48,20 @@ Class Order extends Controller{
     {
         $request = Request::instance();
         $param = $request->param();
+        
+        $order_all = MealOrder::where('store_id',$param['store_id'])->find();
+        
+        if($order_all){
+            
+            $pay = new WechatPay();
+            $data = $pay->get_pay($order_all['id']);
+            
+            //存入微信支付返回参数
+            MealOrder::where('id',$order_all['id'])->update(['wx_pay'=>$data,'wx_time'=>date( 'Y-m-d H:i:s' )]);
 
-        $rules = [
+            $data ? returnJson(1,'成功',$data) : returnJson(0,'失败');
+        }else {
+            $rules = [
             'goods_name' => 'require',
             'goods_quantity'=>'require',
             'amount_money'=>'require',
@@ -62,91 +74,93 @@ Class Order extends Controller{
             'rise'=>'require',
             'price'=>'require',
 
-        ];
-        $message = [
-            'goods_name.require' => '套餐名称不能为空',
-            'goods_quantity.require' => '数量不能为空',
-            'amount_money.require'=>'金额不能为空',
-            'store_id.require'=>'店铺id不能为空',
-            'enter_all_id.require'=>'套餐id不能为空',
-            'user_id.require'=>'用户id不能为空',
-            //发票
-            'type.require'=>'发票类型不能为空',
-            'status.require'=>'发票样式不能为空',
-            'rise.require'=>'抬头不能为空',
-            'price.require'=>'金额不能为空',
-        ];
-        //验证
-        $validate = new Validate($rules,$message);
-        if(!$validate->check($param)){
-            return json(['code' => 0,'msg' => $validate->getError()]);
-        }
-        //查询个人信息
-        $user = new \app\rec\model\User();
-        $user_all = $user->user_index($param['user_id']);
-
-        //查询店铺信息
-        $store_all = Store::where('id',$param['store_id'])->find()->toArray();
-        // 启动事务
-        Db::startTrans();
-        try{
-            //生成订单
-            $order = new MealOrder();
-            $order_list = $order->add($param['user_id'],$param['goods_name'],$param['goods_quantity'],$param['amount_money'],$param['store_id'],$param['enter_all_id'],$store_all['store_name'],$param['price'],$user_all['openid']);
-            $no = $order_list->order_number;
-            $order_id = $order_list->id;
-//            print_r($order_id);die;
-            // 提交事务
-            Db::commit();
-            $type = $param['type'];
-            switch($type){
-                case 1:
-                    if(empty($param['email'])){
-                        $param['email'] = '';
-                    }
-                    if(empty($param['duty'])){
-                        $param['duty'] = '';
-                    }
-                    if(empty($param['address'])){
-                        $param['address'] = '';
-                    }
-
-                    $invoice = new InvoiceAll();
-                    $result = $invoice->add_enterprise($param['user_id'],$no,$type,$param['status'],$param['email'],$param['rise'],$param['duty'],'100',$user_all['phone_number'],$param['address']);
-
-                    $pay = new WechatPay();
-                    $data = $pay->get_pay($order_id);
-
-                    //存入微信支付返回参数
-                    $order -> where('id',$order_id)->update(['wx_pay'=>$data]);
-
-                    $data ? returnJson(1,'成功',$data) : returnJson(0,'失败');
-
-                    break;   // 跳出循环
-                case 2:
-                    if(empty($param['email'])){
-                        $param['email'] = '';
-                    }
-                    if(empty($param['address'])){
-                        $param['address'] = '';
-                    }
-                    $invoice = new InvoiceAll();
-                    $result = $invoice->add_personal($param['user_id'],$no,$type,$param['status'],$param['email'],$param['rise'],$user_all['phone_number'],'100',$param['address']);
-
-                    $pay = new WechatPay();
-                    $data = $pay->get_pay($order_id);
-
-                    //存入微信支付返回参数
-                    $order -> where('id',$order_id)->update(['wx_pay'=>$data]);
-
-                    $data ? returnJson(1,'成功',$data) : returnJson(0,'失败');
-
-                    break;
+            ];
+            $message = [
+                'goods_name.require' => '套餐名称不能为空',
+                'goods_quantity.require' => '数量不能为空',
+                'amount_money.require'=>'金额不能为空',
+                'store_id.require'=>'店铺id不能为空',
+                'enter_all_id.require'=>'套餐id不能为空',
+                'user_id.require'=>'用户id不能为空',
+                //发票
+                'type.require'=>'发票类型不能为空',
+                'status.require'=>'发票样式不能为空',
+                'rise.require'=>'抬头不能为空',
+                'price.require'=>'金额不能为空',
+            ];
+            //验证
+            $validate = new Validate($rules,$message);
+            if(!$validate->check($param)){
+                return json(['code' => 0,'msg' => $validate->getError()]);
             }
-        } catch (\Exception $e) {
-            // 回滚事务
-            Db::rollback();
+            //查询个人信息
+            $user = new \app\rec\model\User();
+            $user_all = $user->user_index($param['user_id']);
+    
+            //查询店铺信息
+            $store_all = Store::where('id',$param['store_id'])->find()->toArray();
+            // 启动事务
+            Db::startTrans();
+            try{
+                //生成订单
+                $order = new MealOrder();
+                $order_list = $order->add($param['user_id'],$param['goods_name'],$param['goods_quantity'],$param['amount_money'],$param['store_id'],$param['enter_all_id'],$store_all['store_name'],$param['price'],$user_all['openid']);
+                $no = $order_list->order_number;
+                $order_id = $order_list->id;
+    //            print_r($order_id);die;
+                // 提交事务
+                Db::commit();
+                $type = $param['type'];
+                switch($type){
+                    case 1:
+                        if(empty($param['email'])){
+                            $param['email'] = '';
+                        }
+                        if(empty($param['duty'])){
+                            $param['duty'] = '';
+                        }
+                        if(empty($param['address'])){
+                            $param['address'] = '';
+                        }
+    
+                        $invoice = new InvoiceAll();
+                        $result = $invoice->add_enterprise($param['user_id'],$no,$type,$param['status'],$param['email'],$param['rise'],$param['duty'],'100',$user_all['phone_number'],$param['address']);
+    
+                        $pay = new WechatPay();
+                        $data = $pay->get_pay($order_id);
+    
+                        //存入微信支付返回参数
+                        $order -> where('id',$order_id)->update(['wx_pay'=>$data,'wx_time'=>date( 'Y-m-d H:i:s' )]);
+    
+                        $data ? returnJson(1,'成功',$data) : returnJson(0,'失败');
+    
+                        break;   // 跳出循环
+                    case 2:
+                        if(empty($param['email'])){
+                            $param['email'] = '';
+                        }
+                        if(empty($param['address'])){
+                            $param['address'] = '';
+                        }
+                        $invoice = new InvoiceAll();
+                        $result = $invoice->add_personal($param['user_id'],$no,$type,$param['status'],$param['email'],$param['rise'],$user_all['phone_number'],'100',$param['address']);
+    
+                        $pay = new WechatPay();
+                        $data = $pay->get_pay($order_id);
+    
+                        //存入微信支付返回参数
+                        $order -> where('id',$order_id)->update(['wx_pay'=>$data]);
+    
+                        $data ? returnJson(1,'成功',$data) : returnJson(0,'失败');
+    
+                        break;
+                }
+            } catch (\Exception $e) {
+                // 回滚事务
+                Db::rollback();
+            }
         }
+        
 
     }
 
