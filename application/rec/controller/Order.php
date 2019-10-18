e4611539592ebf1a284903c22df75c280b4e6016
lilu1233 <lilusiring@163.com>
2019-10-18 16:15:24 +0800

    拉代码

--- application/rec/controller/Order.php
+++ application/rec/controller/Order.php
@@ -164,25 +164,28 @@ Class Order extends Controller{
                 }
                 break;   // 跳出循环
             case 2: //过期订单
-
+            	
+				$param = $this->Verification($param);
+				
                 //查询个人信息
                 $user = new \app\rec\model\User();
                 $user_all = $user->user_index($param['user_id']);
 
                 //查询店铺信息
                 $store_all = Store::where('id', $param['store_id'])->find()->toArray();
+                
                 if(!$store_all)returnJson(0,'店铺有误');
                 // 启动事务
                 Db::startTrans();
                 try {
 
-                    $param = $this->Verification($param);
+                    
                     //生成订单
                     $order = new MealOrder();
                     $order_list = $order->edit($param['store_id'],$param['user_id'], $param['goods_name'], $param['goods_quantity'], $param['amount_money'],  $param['enter_all_id'], $store_all['store_name'], $param['price'], $user_all['openid']);
                     $no = $order_list->order_number;
                     $order_id = $order_list->id;
-                    //            print_r($order_id);die;
+                                print_r($order_id);die;
                     // 提交事务
                     Db::commit();
                     $type = $param['type'];
