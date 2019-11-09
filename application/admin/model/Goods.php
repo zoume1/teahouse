7dee7a10bb4ebf1fcf75bd42c3f58bdfffbff310
guoyangzt <gy156789@163.com>
2019-11-09 18:23:18 +0800

    分销邀请

--- application/admin/model/Goods.php
+++ application/admin/model/Goods.php
@@ -63,5 +63,70 @@ class Goods extends Model
          return  isset($data) ? array_values($data) : null;
         
     }
+
+
+    public function gettoken()
+    {
+        $APPID = "wx301c1368929fdba8";
+        $APPSECRET =  "4bc1912eb2cbab3e7b0bb0990b60036e";
+        $access_token = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
+        $json = $this->httpRequest($access_token);
+        return  json_decode($json,true);
+    }
+
+    //curl
+    public function httpRequest($url, $data='', $method='GET'){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        if($method=='POST')
+        {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data != '')
+            {
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            }
+        }
+
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
+    }
+
+    // //生成二维码
+    // public function qrcode($shopid,$userid)
+    // {
+    //     $ACCESS_TOKEN = $this->gettoken();
+    //     $puthc = '/pages/product/detail?pro_id='.$shopid.'&userid='.$userid;//小程序的路径 可以带参数
+    //     $qcode ="https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=".$ACCESS_TOKEN['access_token'];
+    //     $param = json_encode(array("path"=>$puthc,"width"=> 150));
+    //     $result = $this->httpRequest( $qcode,$param,"POST");
+    //     $puth = "public/qrcode/".time().rand(100000,999999).'.png';
+    //     file_put_contents($puth,$result);
+    //     return $puth;
+    // }
+
+        //生成二维码
+    public function qrcode()
+    {
+        $ACCESS_TOKEN = $this->gettoken();
+        $id =433;
+        $puthc = 'pages/goods_detail/goods_detail?title='.$id;//小程序的路径 可以带参数
+        halt($puthc);
+        $qcode ="https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=".$ACCESS_TOKEN['access_token'];
+        $param = json_encode(array("path"=>$puthc,"width"=> 150));
+        $result = $this->httpRequest( $qcode,$param,"POST");
+        $puth = ROOT_PATH . 'public' . DS . 'uploads'.DS.'D'.time().rand(100000,999999).'.png';
+        
+        file_put_contents($puth,$result);
+        return $puth;
+    }
  
 }
