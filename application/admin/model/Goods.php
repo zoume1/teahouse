87b1daa6524b42681ec7b6e35762e82ecf63a8ef
guoyangzt <gy156789@163.com>
2019-12-23 15:20:50 +0800

    拉代码

--- application/admin/model/Goods.php
+++ application/admin/model/Goods.php
@@ -201,6 +201,6 @@ class Goods extends Model
     {
         if(!iseet($goods_number) || empty($goods_number)) return jsonError('商品编码不能为空');
         $accompany_data = self::get(['goods_number',$goods_number]);
-        return $accompany_data ? $accompany_data->toArray():jsonError('没有该商品编码，请仔细核对再搜索');
+        return $accompany_data ? jsonSuccess($accompany_data->toArray()):jsonError('没有该商品编码，请仔细核对再搜索');
     }
 }
