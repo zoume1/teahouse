a81660cf754cfef20b4ec03197b5fb91dcf74f04
lilu1233 <lilusiring@163.com>
2019-12-25 16:05:39 +0800

    pull

--- public/test.php
+++ public/test.php
@@ -3,10 +3,11 @@
 
 $url=$_GET['url'];
 $data=file_get_contents($url);
+$ip = $_SERVER["REMOTE_ADDR"];
+echo $ip;
+// header('Content-Type:image/jpeg;');
 
-header('Content-Type:image/jpeg;');
-
-echo $date;
+// echo $date;
 
 
 
