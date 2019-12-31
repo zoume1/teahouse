feaab9d95dc8d658cb7118e4690df733ec652184
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-12-31 16:20:40 +0800

    优化订单

--- application/rec/model/MealOrder.php
+++ application/rec/model/MealOrder.php
@@ -16,7 +16,7 @@ class MealOrder extends Model{
     protected $resultSetType = 'collection';
 
     //新增
-    public function add($uid,$no,$name,$quantity,$money,$store_id,$enter_all_id,$store_name,$pay,$openid,$img,$invoice)
+    public function add($uid,$no,$name,$quantity,$money,$store_id,$enter_all_id,$store_name,$pay,$openid,$img,$invoice,$false_data)
     {
         $data = new MealOrder;
         $data->save([
@@ -38,6 +38,7 @@ class MealOrder extends Model{
             "status_type"=>1,//版本开启状态状态（1为正常状态，0为关闭状态）
             "false_data"=>1,//记录
             "invoice"=>$invoice,
+            "false_data"=>$false_data,
             'create_time'=>time()
         ]);
 
@@ -84,6 +85,6 @@ class MealOrder extends Model{
 
     }
 
-    
+
 
 }
