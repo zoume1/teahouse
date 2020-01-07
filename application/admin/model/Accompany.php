bf2dffc54f579f0b2c2b7b8a6fa1125630e42174
guoyangzt <gy156789@163.com>
2020-01-07 11:53:55 +0800

    拉代码

--- application/admin/model/Accompany.php
+++ application/admin/model/Accompany.php
@@ -121,16 +121,16 @@ class Accompany extends Model
 
             }
             $this->commit();
-            //压缩文件
-            $dir_path = ROOT_PATH . 'public' . DS . 'directional'. DS . $this->id . DS ; //想要压缩的目录
-            $zipName = ROOT_PATH . 'public' . DS . 'directional'. DS . $this->id . DS.'test.zip';
+            // //压缩文件
+            // $dir_path = ROOT_PATH . 'public' . DS . 'directional'. DS . $this->id . DS ; //想要压缩的目录
+            // $zipName = ROOT_PATH . 'public' . DS . 'directional'. DS . $this->id . DS.'test.zip';
 
-            $makeZip = new MakeZip();
-            //重复压缩，则会自动覆盖
-            $res = $makeZip->zip($dir_path,$zipName,$this->id);
-            if(!$res){
-                throw new Exception('压缩失败');
-            } 
+            // $makeZip = new MakeZip();
+            // //重复压缩，则会自动覆盖
+            // $res = $makeZip->zip($dir_path,$zipName,$this->id);
+            // if(!$res){
+            //     throw new Exception('压缩失败');
+            // } 
             return true;
         } catch (\Exception $e) {
             $this->error = $e->getMessage();
