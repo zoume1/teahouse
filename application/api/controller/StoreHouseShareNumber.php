77d4fe98d99ffe128b8a76a1e77936ac51f2a458
guoyangzt <gy156789@163.com>
2020-01-11 14:47:57 +0800

    拉代码

--- application/api/controller/StoreHouseShareNumber.php
+++ application/api/controller/StoreHouseShareNumber.php
@@ -11,6 +11,7 @@ use \think\Exception;
 use app\admin\model\Goods;
 use think\Validate;
 use app\admin\model\ShareOrder;
+use app\index\controller\Storehouse;
 
 /**
  * 茶仓分享存茶
@@ -87,7 +88,57 @@ class StoreHouseShareNumber extends Controller
             if (!$validate->check($data)) {
                 $error = $validate->getError();
                 return jsonError($error);
-            } 
+            }
+        }
+    }
+
+    /**
+     * 茶仓赠茶点击赠送页面
+     * @param \think\Model $houseorder
+     * @string 存茶订单id
+     * @return array
+     * @throws \app\common\exception\BaseException
+     * @throws \think\Exception
+     * @throws \think\exception\DbException
+     */
+    public function ShowOrderNumber(Request $request)
+    {
+        if ($request->isPost()) {
+            $data = input();
+            $validate  = new Validate([
+                ['id', 'require', '订单id不能为空'],
+                ['lowest', 'require', '最大出仓数量不能为空'],
+                ['out_number', 'require', '赠送数量不能为空'],
+                ['lowest_unit', 'lowest_unit', '出仓单位不能为空'],
+            ]);
+            //验证部分数据合法性
+            if (!$validate->check($data)) {
+                $error = $validate->getError();
+                return jsonError($error);
+            }
+            $surplus = intval($data['lowest'] - $data['number']);
+            $house_order = HouseOrder::getHouseOrder($data['id']);
+            if (!$house_order)  return jsonError('该订单不存在');
+            if (!empty($house_order['special_id'])) {
+                $goods = Db::name("special")->where("id", $house_order['special_id'])->find();
+            } else {
+                $goods = Db::name("goods")->where("id", $house_order['goods_id'])->find();
+                if (empty($goods)) return jsonError('商品已下架');
+            }
+
+            $unit = explode(",", $goods['unit']);
+            $num = explode(",", $goods['num']);
+            //1.计算出仓数量等价单位
+            $string_number = (new Storehouse())->getComputeUnit($data['out_number'], $num, $unit);
+            //剩余单位数量
+            $surplus_number = (new Storehouse())->getComputeUnit($surplus, $num, $unit);
+            $rest_data = [
+                'string_number' => explode(',', $string_number),
+                'surplus_number' => explode(',', $surplus_number),
+                'lowest_unit' => $data['lowest_unit'],
+            ];
+
+            return jsonSuccess('发送成功', $rest_data);
         }
     }
 
@@ -100,14 +151,56 @@ class StoreHouseShareNumber extends Controller
      * @throws \think\Exception
      * @throws \think\exception\DbException
      */
-    public function ShowOrderNumber(Request $request)
+    public function cLickGive(Request $request)
     {
         if ($request->isPost()) {
-            $order_id = $request->only(['id'])['id'];
-            
-           
+            $data = input();
+            $validate  = new Validate([
+                ['store_number', 'require', '订单id不能为空'],
+                ['id', 'require', '订单id不能为空'],
+            ]);
+            //验证部分数据合法性
+            if (!$validate->check($data)) {
+                $error = $validate->getError();
+                return jsonError($error);
+            }
+            $house_order = HouseOrder::getHouseOrder($data['id']);
+            if (!$house_order)  return jsonError('该订单不存在');
+            if (!empty($house_order['special_id'])) {
+                $goods = Db::name("special")->where("id", $house_order['special_id'])->find();
+            } else {
+                $goods = Db::name("goods")->where("id", $house_order['goods_id'])->find();
+                if (empty($goods)) return jsonError('商品已下架');
+            }
+            $store_number = $data['store_number'];
+            $unit = explode(",", $goods['unit']);
+            $num = explode(",", $goods['num']);
+            //前端要限制最低单位出仓数量
+            $count = count($unit);
+            switch ($count) {
+                case RESTEL_ONE:
+                    $lowest = $store_number[RESTEL_ZERO];
+                    $lowest_unit = $unit[RESTEL_ZERO];
+                    break;
+                case RESTEL_TWO:
+                    $lowest_unit = $unit[RESTEL_ONE];
+                    $lowest = intval($store_number[RESTEL_ZERO]) * intval($num[RESTEL_ONE]) + intval($store_number[RESTEL_TWO]);
+                    break;
+                case RESTEL_THREE:
+                    $lowest_unit = $unit[RESTEL_TWO];
+                    $Replacement = intval(intval($num[RESTEL_TWO]) / intval($num[RESTEL_ONE]));
+                    $lowest = intval($store_number[RESTEL_ZERO]) * intval($num[RESTEL_TWO]) + intval($store_number[RESTEL_TWO]) * $Replacement + intval($store_number[RESTEL_FOUR]);;
+                    break;
+                default:
+                    $lowest = 0;
+                    break;
+            }
+            //剩余单位数量
+            $rest_data = [
+                'lowest' => $lowest,
+                'lowest_unit' => $lowest_unit,
+            ];
+            return jsonSuccess('发送成功', $rest_data);
         }
     }
-
-    
 }
