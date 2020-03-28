4eced136809b2100db27c28f546dae58c8c39d91
Zheng-jp <970663468@qq.com>
2020-03-28 16:46:47 +0800

    pull

--- application/database.php
+++ application/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'        => 'siring',
     // 密码
-    'password'        => 'ei%&#w9l3z3',
+    'password'        => '&@5757ei%&#w9l3z3',
     // 端口
     'hostport'        => '',
     // 连接dsn
--- public/xeixin/mingtiancha/mingtiancha.hyperesources/index.js
+++
@@ -1,8 +0,0 @@
-document.write("<script type='text/javascript' src='https://cdn.bootcss.com/jquery/3.4.1/jquery.js'></script>");
-document.write("<script type='text/javascript' src='https://res.wx.qq.com/open/js/jweixin-1.3.2.js'></script>");
-
-function toWechat() {
-    wx.miniProgram.switchTab({
-        url: '/pages/storage/view/view'
-    });
-}
